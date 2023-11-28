533042f859ef901e6bbf68a966d260a56bd80a92
Ayan-farhad <khaanayan81@gmail.com>
2023-11-28 23:47:23 +0500

    StopWatch

--- app.js
+++ app.js
@@ -181,8 +181,8 @@
 
 
 // let arr = ['usama', 'ali' , 'ayan', 'sheraz']
-// let arr2 = ['javed','khan']                  normal
-// for (let i=0; i<arr.length; i++){
+// let arr2 = ['javed','khan']                  
+// for (let i=0; i<arr.length; i++){              Normal
 //     document.write(arr[i]+' '+ arr2+'<br>')
 // }
 
@@ -205,13 +205,99 @@
 
 // ------------------------------WHILE-LOOP-------------------------------------------------
 
-// let a = 1
-// while (a <= 10){
+// let a = 10
+// while (a <= 20){
 //     console.log("chal raha hai");
 //     a++;
 // }
 
+
 // ---------------------------------DO-LOOP----------------------------------------------------
+
+// let a = 5
+
+// do {
+//      console.log("Do chal raha " +a)
+//      a++;
+    
+// } while (a<=10);
+
+
+// -------------------------------------FOR-OF-LOOP---------------------------------------------------
+
+// let str = "Ayan"
+
+// for (let u of str) {
+//     console.log("u =", u);
+// }
+
+
+// --------------------------------------METHODS------------------------------------------------------
+
+// setInterval(function(){                      
+//     document.write("Ayan ")        time dene ke bad chalta hi rahega
+// } ,2000)
+
+// function timer(){
+//     document.write("ayan");
+// } setInterval(timer,2000)
+
+// setTimeout(function(){
+//     document.write("Chal na")     time dene ke bad aik hi bar chalega 
+// },4000)
+
+
+
+// let a = 1
+// setInterval(function(){
+//     a++
+//     console.log(a);
+// },500);
+
+
+var minutes = 0
+var seconds = 0
+var miliseconds = 0
+var getmin = document.getElementById('min')
+var getsec = document.getElementById('sec')
+var getmili = document.getElementById('msec')
+var interval;
+
+
+
+function start(){
+    interval = setInterval(function(){
+        miliseconds++
+        getmili.innerHTML = miliseconds
+        if(miliseconds >= 100){
+            seconds++
+            getsec.innerHTML = seconds
+            miliseconds = 0
+        }
+        else if(seconds >= 5){
+            minutes++
+            getmin.innerHTML = minutes
+            seconds = 0
+        }
+    },10)  
+    document.getElementById('sss').disabled = true
+}
+function stop(){
+    clearInterval(interval)
+    document.getElementById('sss').disabled = false
+}
+
+function reset(){
+    minutes = 0
+    miliseconds = 0
+    seconds = 0
+    getmin.innerHTML = minutes
+    getsec.innerHTML = seconds
+    getmili.innerHTML = miliseconds
+}
+
+
+
 
 
 
@@ -797,8 +883,8 @@
 // })
 
 
-let email = document.getElementById("email")
-let password = document.getElementById("Password")
-let btn =  document.getElementById("btn")
+// let email = document.getElementById("email")
+// let password = document.getElementById("Password")
+// let btn =  document.getElementById("btn")
 
-btn.addEventListener("click" )
+// btn.addEventListener("click" )
